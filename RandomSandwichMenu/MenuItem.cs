5de7a583b50dec9a25f30ee05a3078120da3c826
Bartek <bartlomiej.kacki@gmail.com>
2024-06-01 19:29:58 +0200

    Add project files.

---
+++ RandomSandwichMenu/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSandwichMenu
+{
+    class MenuItem
+    {
+        public static Random Randomizer = new Random();
+
+        public string[] Proteins = { "Wołowina", "Salami", "Indyk", "Szynka", "Pastrami", "Tofu" };
+        public string[] Condiments = { "musztarda żółta", "musztarda brązowa", "musztarda miodowa", "majonez", "przyprawy", "sos francuski" };
+        public string[] Breads = { "ryżowe", "białe", "pszenne", "pumpernikiel", "bułka" };
+
+        public string Description = "";
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+
+            Description = randomProtein + " i " + randomCondiment + ". Pieczywo: " + randomBread + ".";
+
+            //ten fragment kodu generuje losową cenę z przedziału 2,01 do 4,97 i wyświetla jako walutę
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+        }
+    }
+}
